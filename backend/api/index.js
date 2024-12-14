cabb70ce78bd5d9bef2fa56b9f6064a4e83d44c0
tracy <200548417@student.georgianc.on.ca>
2024-12-13 22:36:48 -0500

    8

--- backend/api/index.js
+++ backend/api/index.js
@@ -61,6 +61,9 @@ app.use(cookieParser());
 //     maxAge: 24 * 60 * 60 * 1000, // 1 day
 //   },
 // }));
+
+const isProduction = process.env.NODE_ENV === 'production';
+
 app.use(session({
   secret: "secretSession",
   resave: false,
